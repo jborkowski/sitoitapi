c78f0a7c03b0af213841b037d427a59f20996f55
Jonatan Borkowski <jonatan.borkowski@pm.me>
2021-01-18 00:31:38 +0100

    Add missing methods for flashcard (without error handling)

--- src/App.hs
+++ src/App.hs
@@ -4,14 +4,15 @@ module App where
 
 import API (API, api)
 import Data.Pool (Pool)
-import Servant (Server, serve)
+import Servant (Server, serve, (:<|>) ((:<|>)))
 import Database.PostgreSQL.Simple (Connection)
-import Flashcard.API (create)
+import Flashcard.API (create, getAll, getById, update, delete)
 import Network.Wai.Handler.Warp (run)
 import Data.Text (Text)
 
+-- Add custom AppM
 server :: Pool Connection -> Server API
-server conns = create conns
+server conns = create conns :<|> getAll conns :<|> getById conns :<|> update conns :<|> delete conns
 
 runApp :: Pool Connection -> Int -> Text -> IO ()
 runApp coons port host = run port (serve api $ server coons)
--- src/Flashcard/API.hs
+++ src/Flashcard/API.hs
@@ -9,14 +9,19 @@ module Flashcard.API where
 
 import Servant
 import Flashcard.Types
-import Data.Pool (Pool, withResource)
-import Database.PostgreSQL.Simple (Connection, query, Query)
+import Data.Pool (Pool, withResource, takeResource, putResource)
+import Database.PostgreSQL.Simple (Connection, query, query_, Query, Only(..), execute, begin, commit)
 import Database.PostgreSQL.Simple.SqlQQ
 import Control.Monad.IO.Class (liftIO)
+import Data.UUID (UUID)
 
 type CreateFlashcard = ReqBody '[JSON] FlashcardRequest :> PostCreated '[JSON] Flashcard
+type GetFlashcards = Get '[JSON] [Flashcard]
+type GetFlashcard = Capture "flashcardid" UUID :> Get '[JSON] Flashcard
+type UpdateFlashcard = Capture "flashcardid" UUID :> ReqBody '[JSON] FlashcardRequest :> PutAccepted '[JSON] Flashcard
+type DeleteFlashcard = Capture "flashcardid" UUID :> DeleteNoContent
 
-type FlashcardAPI = "flashcard" :> CreateFlashcard
+type FlashcardAPI = "flashcard" :> (CreateFlashcard :<|> GetFlashcards :<|> GetFlashcard :<|> UpdateFlashcard :<|> DeleteFlashcard)
 
 flashcardAPI :: Proxy FlashcardAPI
 flashcardAPI = Proxy
@@ -27,6 +32,33 @@ create conns FlashcardRequest{..} =
     [flashcard] <- query conn insertSQL (requestCategory, requestQuestion, requestAnswer, requestStyle)
     pure flashcard
 
+getAll :: Pool Connection -> Handler [Flashcard]
+getAll coons =
+  liftIO . withResource coons $ \conn -> query_ conn getAllSQL
+
+getById :: Pool Connection -> UUID -> Handler Flashcard
+getById coons uuid =
+  liftIO . withResource coons $ \conn -> do
+  [flashcard] <- query conn getByIdSQL (Only uuid)
+  pure flashcard
+
+update :: Pool Connection -> UUID -> FlashcardRequest -> Handler Flashcard
+update coons uuid FlashcardRequest{..} =
+  liftIO . withResource coons $ \conn -> do
+  begin conn
+  n <- execute conn updateSQL (requestCategory, requestQuestion, requestAnswer, requestStyle, uuid)
+  [flashcard] <- query conn getByIdSQL (Only uuid)
+  commit conn
+  pure flashcard
+  -- Add Errors handling
+  -- throwError err400 {errBody = "Incorrect flashcard id"}
+
+delete :: Pool Connection -> UUID -> Handler NoContent
+delete coons uuid =
+  liftIO . withResource coons $ \conn -> do
+  _ <- execute conn deleteSQL (Only uuid)
+  pure NoContent
+
 insertSQL :: Query
 insertSQL = [sql|
   INSERT INTO flashcard (id, category, question, answer, style, created_at)
@@ -34,3 +66,30 @@ insertSQL = [sql|
   RETURNING id, question, answer, category, style, created_at
 |]
 
+updateSQL :: Query
+updateSQL = [sql|
+  UPDATE flashcard
+  SET category = ?, question = ?, answer = ?, style = ?
+  WHERE id = ? AND archived_at IS NULL
+|]
+
+getAllSQL:: Query
+getAllSQL = [sql|
+  SELECT id, question, answer, category, style, created_at
+  FROM flashcard
+  WHERE archived_at IS NULL
+|]
+
+getByIdSQL :: Query
+getByIdSQL = [sql|
+  SELECT id, question, answer, category, style, created_at
+  FROM flashcard
+  WHERE id = ? AND archived_at IS NULL
+|]
+
+deleteSQL :: Query
+deleteSQL = [sql|
+  UPDATE flashcard
+  SET archived_at = now()
+  WHERE id = ? AND archived_at IS NULL
+|]
